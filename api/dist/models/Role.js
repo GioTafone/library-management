a35a99843cf83875dcc74aaff3d6c96a83c2cdd8
Giovanni Tafone <giola@Giovannis-MacBook-Air.local>
2023-02-21 10:46:10 +0200

    roles

--- api/dist/models/Role.js
+++ api/dist/models/Role.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const types_1 = require("../types");
-const roleSchema = new mongoose_1.default.Schema({
+const chema = new mongoose_1.default.Schema({
     roleName: {
         type: String,
         required: true,
--- client/src/roles.js
+++
@@ -1,2 +0,0 @@
-export const adminRoleId = "633c39d5c99c1fb4c90db991";
-export const userRoleId = "633c251c104509110d12e79d";
